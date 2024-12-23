1b605fc28d293f4b8b8287e0547a3f291d2ff6d5
matinraisi <raisi.m630@gmail.com>
2024-12-23 09:54:51 +0330

    marge fron & homapage and settind template , base footer and header

--- conf/urls.py
+++ conf/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.conf.urls.static import static 
+from django.conf import settings
 
 
 urlpatterns = [
@@ -25,3 +26,6 @@ urlpatterns = [
     path('authentication/', include('authentication.urls',namespace= "authentication")),
     
 ]
+
+urlpatterns += static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL , document_root=settings.STATIC_ROOT)
